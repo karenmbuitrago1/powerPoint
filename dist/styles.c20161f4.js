017dd5ce91b87a56cc3d34218cfbf43ebaab55c2
karenmbuitrago1 <karenmbuitrago@gmail.com>
2022-11-08 03:16:31 -0500

    add fase 7

--- dist/styles.c20161f4.js
+++ dist/styles.c20161f4.js
@@ -228,7 +228,11 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\recurso_7.png":[["recurso_7.5b9994e7.png","../images/recurso_7.png"],"../images/recurso_7.png"],"./..\\images\\recurso_16.png":[["recurso_16.94b2fa25.png","../images/recurso_16.png"],"../images/recurso_16.png"],"_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../styles/fase8.css":[function(require,module,exports) {
+},{"./..\\images\\recurso_7.png":[["recurso_7.5b9994e7.png","../images/recurso_7.png"],"../images/recurso_7.png"],"./..\\images\\recurso_16.png":[["recurso_16.94b2fa25.png","../images/recurso_16.png"],"../images/recurso_16.png"],"_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../styles/fase7.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./..\\images\\recurso_190.png":[["recurso_190.19353560.png","../images/recurso_190.png"],"../images/recurso_190.png"],"./..\\images\\recurso95.png":[["recurso95.8b88286d.png","../images/recurso95.png"],"../images/recurso95.png"],"./..\\images\\recurso97.png":[["recurso97.fba8b7e3.png","../images/recurso97.png"],"../images/recurso97.png"],"_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../styles/fase8.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
@@ -236,7 +240,7 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"../styles/global.css":"../styles/global.css","../styles/intro.css":"../styles/intro.css","../styles/intro-card.css":"../styles/intro-card.css","../styles/intro-route.css":"../styles/intro-route.css","../styles/fase1.css":"../styles/fase1.css","../styles/fase1-post.css":"../styles/fase1-post.css","../styles/fase1-herramientas.css":"../styles/fase1-herramientas.css","../styles/fase1-tips.css":"../styles/fase1-tips.css","../styles/fase2.css":"../styles/fase2.css","../styles/fase2-herramientas.css":"../styles/fase2-herramientas.css","../styles/fase3.css":"../styles/fase3.css","../styles/fase3-post.css":"../styles/fase3-post.css","../styles/fase4.css":"../styles/fase4.css","../styles/fase5.css":"../styles/fase5.css","../styles/fase6.css":"../styles/fase6.css","../styles/fase8.css":"../styles/fase8.css","_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"../styles/global.css":"../styles/global.css","../styles/intro.css":"../styles/intro.css","../styles/intro-card.css":"../styles/intro-card.css","../styles/intro-route.css":"../styles/intro-route.css","../styles/fase1.css":"../styles/fase1.css","../styles/fase1-post.css":"../styles/fase1-post.css","../styles/fase1-herramientas.css":"../styles/fase1-herramientas.css","../styles/fase1-tips.css":"../styles/fase1-tips.css","../styles/fase2.css":"../styles/fase2.css","../styles/fase2-herramientas.css":"../styles/fase2-herramientas.css","../styles/fase3.css":"../styles/fase3.css","../styles/fase3-post.css":"../styles/fase3-post.css","../styles/fase4.css":"../styles/fase4.css","../styles/fase5.css":"../styles/fase5.css","../styles/fase6.css":"../styles/fase6.css","../styles/fase7.css":"../styles/fase7.css","../styles/fase8.css":"../styles/fase8.css","_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -261,7 +265,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63455" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64242" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
