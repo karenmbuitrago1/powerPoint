a3a1f757c5dd58668751a03a005303102824ad9a
karenmbuitrago1 <karenmbuitrago@gmail.com>
2022-11-07 23:49:43 -0500

    lazyload imagenes

--- dist/styles.c20161f4.js
+++ dist/styles.c20161f4.js
@@ -233,7 +233,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60497" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60922" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
