5453475bcaa42fec862e58e7dc74c5ff9c62927b
karenmbuitrago1 <karenmbuitrago@gmail.com>
2022-11-08 20:59:29 -0500

    finaliza fase 8

--- dist/styles.c20161f4.js
+++ dist/styles.c20161f4.js
@@ -236,7 +236,7 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\recurso_229.png":[["recurso_229.01d25726.png","../images/recurso_229.png"],"../images/recurso_229.png"],"./..\\images\\recurso_244.png":[["recurso_244.dafeb43b.png","../images/recurso_244.png"],"../images/recurso_244.png"],"./..\\images\\recurso_144.png":[["recurso_144.59b73b90.png","../images/recurso_144.png"],"../images/recurso_144.png"],"_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../styles/fase8-herramientas.css":[function(require,module,exports) {
+},{"./..\\images\\recurso_229.png":[["recurso_229.01d25726.png","../images/recurso_229.png"],"../images/recurso_229.png"],"./..\\images\\recurso_244.png":[["recurso_244.dafeb43b.png","../images/recurso_244.png"],"../images/recurso_244.png"],"./..\\images\\recurso_144.png":[["recurso_144.59b73b90.png","../images/recurso_144.png"],"../images/recurso_144.png"],"./..\\images\\recurso_238.png":[["recurso_238.f2b052e9.png","../images/recurso_238.png"],"../images/recurso_238.png"],"_css_loader":"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../styles/fase8-herramientas.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
